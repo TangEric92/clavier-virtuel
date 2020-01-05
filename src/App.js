d85c0bf8b7bcd6e294a7dfce3255b97620cb2f53
Eric <you@example.com>
2020-01-05 19:32:55 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [maj, setMaj] = useState(false);
+
+  const rows1 = ["a", "z", "e", "r", "t", "y", "u", "i", "o", "p", "<X|"];
+  const rows2 = ["q", "s", "d", "f", "g", "h", "j", "k", "l", "m"];
+  const rows3 = ["MAJ", "w", "x", "SPACE", "c", "v", "b", "n"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="button"
+        onClick={() => {
+          setText("");
+        }}
+      >
+        Restart
+      </button>
+      <header className="header">{text}</header>
+      <div className="line">
+        {rows1.map(letter => {
+          return (
+            <span
+              className="letter"
+              onClick={() => {
+                {
+                  if (letter === "<X|") {
+                    letter = "";
+                    setText(text.slice(0, -1));
+                  } else {
+                    if (maj) {
+                      setText(text + letter.toUpperCase());
+                    } else {
+                      setText(text + letter);
+                    }
+                  }
+                }
+              }}
+            >
+              {" "}
+              {!maj ? letter : letter.toUpperCase()}{" "}
+            </span>
+          );
+        })}
+      </div>
+      <div className="line">
+        {rows2.map(letter => {
+          return (
+            <span
+              className="letter"
+              onClick={() => {
+                {
+                  if (maj) {
+                    setText(text + letter.toUpperCase());
+                  } else {
+                    setText(text + letter);
+                  }
+                }
+              }}
+            >
+              {" "}
+              {!maj ? letter : letter.toUpperCase()}{" "}
+            </span>
+          );
+        })}
+      </div>
+      <div className="line">
+        {rows3.map(letter => {
+          return (
+            <span
+              className="letter"
+              onClick={() => {
+                if (letter === "MAJ") {
+                  setMaj(!maj);
+                  letter = "";
+                }
+                if (letter === "SPACE") {
+                  letter = " ";
+                }
+                if (maj) {
+                  setText(text + letter.toUpperCase());
+                } else {
+                  setText(text + letter);
+                }
+              }}
+            >
+              {!maj ? letter : letter.toUpperCase()}{" "}
+            </span>
+          );
+        })}
+      </div>
+      <div></div>
     </div>
   );
 }
